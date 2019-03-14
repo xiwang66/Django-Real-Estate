5c5bc475527e8ac53fa3f9f0f4566caeec6c8d4f
Xi Wang <wangx272@mcmaster.ca>
2019-03-14 17:47:35 -0400

    remove email settings

--- xi/settings.py
+++ xi/settings.py
@@ -143,13 +143,6 @@ MESSAGE_TAGS = {
 }
 
 
-# Email Config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'example@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
